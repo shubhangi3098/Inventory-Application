b08a46ece8ee3b8033697193768e0f41c82980e8
Shubhangi Jindal <Shubhangi@softobiz.com>
2022-11-11 12:44:21 +0530

    controllers and route

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -23,6 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
+
+//view engine setup for pug
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
